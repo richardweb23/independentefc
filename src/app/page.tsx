801055caaeb689b13c9e10d43a79693afd401926
Richard Quintanilha <richardweb23@gmail.com>
2025-03-18 12:45:39 -0300

    inicia

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="xl:max-w-7xl lg:max-w-5xl md:max-w-3xl sm:max-w-full w-full px-10 shadow-lg py-10 bg-black">
+
+    </div>
+  );
+}
